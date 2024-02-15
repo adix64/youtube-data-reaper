83e61c67d853fa2bb31ff3378933a5c6c9dd7bde
alex.dinu <adix64@gmail.com>
2024-02-15 17:42:38 +0200

    wow

--- tkinterapp.py
+++ tkinterapp.py
@@ -1,34 +1,17 @@
 import tkinter as tk
+from tkinter.font import Font
 from tkinter import ttk
+import webbrowser
 import googleapiclient.discovery
-import googleapiclient.errors
 
-# Function to get video statistics
-def get_video_statistics(youtube, video_id):
-    video_request = youtube.videos().list(
-        part="statistics",
-        id=video_id
-    )
-    video_response = video_request.execute()
-    return video_response['items'][0]['statistics']
+# Function to open video link in the browser, now explicitly receiving the video link as a parameter
+def open_video_link(video_link):
+    webbrowser.open(video_link)
 
-# Function to get channel statistics
-def get_channel_statistics(youtube, channel_id):
-    channel_request = youtube.channels().list(
-        part="statistics",
-        id=channel_id
-    )
-    channel_response = channel_request.execute()
-    return channel_response['items'][0]['statistics']
-
-# Function to execute YouTube search and display results
+# Function to execute YouTube search and update the table
 def youtube_search():
     api_key = "AIzaSyA46ehaE6ov71md0No8ZfIjfWFB4z-X4h8"  # Replace with your API key
-    api_service_name = "youtube"
-    api_version = "v3"
-
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey=api_key)
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api_key)
 
     query = search_entry.get()
     max_results = int(results_spinbox.get())
@@ -36,33 +19,33 @@ def youtube_search():
     request = youtube.search().list(
         part="snippet",
         maxResults=max_results,
-        q=query
+        q=query,
+        type="video"
     )
     response = request.execute()
 
-    videos_data = []
+    for item in magic_table.get_children():
+        magic_table.delete(item)
 
     for item in response['items']:
         video_id = item['id']['videoId']
+        video_title = item['snippet']['title']
         channel_id = item['snippet']['channelId']
+        channel_title = item['snippet']['channelTitle']
+        video_link = f"https://www.youtube.com/watch?v={video_id}"
 
-        video_stats = get_video_statistics(youtube, video_id)
-        channel_stats = get_channel_statistics(youtube, channel_id)
+        video_stats = youtube.videos().list(part="statistics", id=video_id).execute()['items'][0]['statistics']
+        channel_stats = youtube.channels().list(part="statistics", id=channel_id).execute()['items'][0]['statistics']
 
-        videos_data.append({
-            'videoId': video_id,
-            'viewCount': int(video_stats.get('viewCount', 0)),
-            'likeCount': video_stats.get('likeCount'),
-            'channelId': channel_id,
-            'subscriberCount': channel_stats.get('subscriberCount')
-        })
-
-    sorted_videos = sorted(videos_data, key=lambda x: int(x['likeCount'] or 0), reverse=True)
-
-    results_text.delete('1.0', tk.END)
-    for video in sorted_videos:
-        video_id = video['videoId']
-        results_text.insert(tk.END, f"{video}\nhttps://www.youtube.com/watch?v={video_id}\n\n")
+        magic_table.insert("", tk.END, values=(
+            video_title, 
+            video_stats.get('likeCount'), 
+            video_stats.get('viewCount'),
+            channel_title,
+            channel_stats.get('subscriberCount'),
+            item['snippet']['description'],
+            video_link  # Store video link in the last column, which will be hidden
+        ))
 
 # Setting up the Tkinter window
 root = tk.Tk()
@@ -84,8 +67,25 @@ results_spinbox.pack()
 search_button = ttk.Button(root, text="Search", command=youtube_search)
 search_button.pack()
 
-# Results display
-results_text = tk.Text(root, height=20, width=80)
-results_text.pack()
+# Magic Table setup
+columns = ("Video Name", "Like Count", "View Count", "Channel Name", "Subscriber Count", "Description", "URL")
+magic_table = ttk.Treeview(root, columns=columns, show="headings")
+magic_table.pack(expand=True, fill="both")
+
+# Configuring column headings
+for col in columns[:-1]:  # Exclude the URL column from headings
+    magic_table.heading(col, text=col)
+    magic_table.column(col, width=Font().measure(col.title()))
+
+# Hiding the URL column
+magic_table.column("URL", width=0, stretch=False, minwidth=0)
+
+# Modified binding to pass the video link explicitly
+def on_item_click(event):
+    selected_item = magic_table.selection()[0]  # Get selected item
+    video_link = magic_table.item(selected_item, 'values')[6]  # URL is the last value
+    open_video_link(video_link)
+
+magic_table.bind("<Double-1>", on_item_click)
 
 root.mainloop()
